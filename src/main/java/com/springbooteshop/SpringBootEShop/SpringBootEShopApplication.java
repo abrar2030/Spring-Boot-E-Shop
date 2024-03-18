555ae678a9cc723998e0212212a389e4d0321696
Abrar Ahmed <oc7ip4@inf.elte.hu>
2024-03-18 21:18:58 +0100

    Initialization

---
+++ src/main/java/com/springbooteshop/SpringBootEShop/SpringBootEShopApplication.java
@@ -0,0 +1,13 @@
+package com.springbooteshop.SpringBootEShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbooteshop/SpringBootEShop/SpringBootEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbooteshop.SpringBootEShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
